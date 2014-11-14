081e104ced48dc17cff75c8822fd1d1a9dd67635
jeffrichley <jeffrichley@gmail.com>
2014-11-14 14:45:47 -0500

    documentation

--- src/main/java/com/infinity/bumblebee/training/net/PredictionEvaluator.java
+++ src/main/java/com/infinity/bumblebee/training/net/PredictionEvaluator.java
@@ -4,11 +4,21 @@ import com.infinity.bumblebee.data.BumbleMatrix;
 import com.infinity.bumblebee.data.BumbleMatrixFactory;
 import com.infinity.bumblebee.network.NeuralNet;
 
+/**
+ * Gives various statistics about the evaluation of a <code>NeuralNet</code>'s ability for prediction
+ * @author Jeffrey.Richley
+ */
 public class PredictionEvaluator {
 
 	private int answerCorrect = 0;
 	private int aswersAttempted = 0;
 
+	/**
+	 * Constructor that performs evaluation
+	 * @param input The input examples to predict with
+	 * @param output The expected output of the predictions
+	 * @param net The <code>NeuralNet</code> that is used to perform predictions
+	 */
 	public PredictionEvaluator(BumbleMatrix input, BumbleMatrix output, NeuralNet net) {
 		final BumbleMatrixFactory factory = new BumbleMatrixFactory();
 		
@@ -28,6 +38,10 @@ public class PredictionEvaluator {
 		}
 	}
 
+	/**
+	 * Gives a straight percentage of predictions that were correct
+	 * @return A straight percentage of predictions that were correct
+	 */
 	public double getPercentageCorrect() {
 		return ((double) answerCorrect / (double) aswersAttempted) * 100;
 	}
